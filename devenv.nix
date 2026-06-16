8e85a5b171c971be648d726723c18375b8d548f3
Johan Larsson <johan@jolars.co>
2026-06-16 20:39:01 +0200

    chore: setup devenv

--- devenv.nix
+++ devenv.nix
@@ -1,46 +1,41 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  packages = [
+    pkgs.bashInteractive
+    pkgs.perf
+    pkgs.cargo-flamegraph
+    pkgs.cargo-llvm-cov
+    pkgs.cargo-audit
+    pkgs.cargo-deny
+    pkgs.go-task
+    pkgs.wasm-pack
+    pkgs.dprint
+    pkgs.dprint-plugins.dprint-plugin-toml
+  ];
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  languages = {
+    rust = {
+      enable = true;
+      toolchainFile = ./rust-toolchain.toml;
+    };
+  };
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  git-hooks = {
+    hooks = {
+      clippy = {
+        enable = false;
+        settings = {
+          allFeatures = true;
+        };
+      };
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+      rustfmt = {
+        enable = true;
+      };
+    };
+  };
 }
